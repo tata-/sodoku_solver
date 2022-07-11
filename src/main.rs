7bc68606e8a7bcb4dcde4c86bf2a071449a99aae
Andras <tata@metacortex.hu>
2022-07-11 21:49:07 +0200

    Get rid-off egui as image handlig was sooo slooow....

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 
-use eframe::egui;
-use eframe::egui::Context;
-use eframe::epi::Frame;
 use std::borrow::{Borrow, BorrowMut};
 
 use opencv::prelude::VideoCaptureTrait;
@@ -11,10 +8,10 @@ use opencv::videoio::VideoCapture;
 use opencv::imgcodecs;
 use opencv::prelude::MatTraitManual;
 
-use egui::{ColorImage, Color32};
 use opencv::imgproc::{cvt_color, COLOR_BGR2RGBA};
 use std::ops::Mul;
 use std::time::{SystemTime, UNIX_EPOCH};
+use opencv::highgui::{named_window, WINDOW_AUTOSIZE, wait_key, imshow, destroy_all_windows};
 
 #[path = "model/model.rs"] mod model;
 
@@ -22,25 +19,31 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     println!("Hello, world!");
-    build_ui();
+    let mut app = SolverApp::default();
+    app.build_ui();
 
 }
 
-fn build_ui(){
-    let options  = eframe::NativeOptions::default();
-    eframe::run_native( Box::new( Box::new(SolverApp::default())), options);
-}
 
-fn getNow() -> u128 {
-    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
-}
 
 struct SolverApp {
     name: String,
     cam: VideoCapture,
     frame: Mat,
-    rgba_frame: Mat,
 }
+
+impl SolverApp{
+    fn build_ui(&mut self){
+        named_window("Camera", WINDOW_AUTOSIZE);
+        while (wait_key(33).unwrap() != 27) {
+            self.cam.read(&mut self.frame);
+            imshow("Camera", &self.frame);
+        }
+
+        destroy_all_windows();
+    }
+}
+
 
 impl Default for SolverApp{
     fn default() -> Self {
@@ -48,64 +51,7 @@ impl Default for SolverApp{
             name: "Name".to_owned(),
             cam : videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(),
             frame : Mat::default(),
-            rgba_frame: Mat::default(),
         }
     }
-}
-
-impl eframe::epi::App for Box<SolverApp>{
-    fn update(&mut self, ctx: &Context, _frame: &Frame) {
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-
-            let start = getNow();
-
-            ui.heading("Sodoku Solver App");
-
-            ui.horizontal( |ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-
-            if ui.button("Reset Name").clicked(){
-                self.name="Sandor".to_owned();
-            }
-
-            ui.label(format!("Hello '{} !!' ", self.name));
-
-            println!("Before read {:?}", getNow() - start);
-            self.cam.read(&mut self.frame);
-            println!("After read {:?}", getNow() - start);
-
-
-            let size = self.frame.size().unwrap();
-
-            let w =  size.width as usize;
-            let h = size.height as usize;
-
-
-
-            cvt_color(&self.frame, self.rgba_frame.borrow_mut(), COLOR_BGR2RGBA, 4);
-            println!("transform done {:?}", getNow()-start);
-
-            let img=ColorImage::from_rgba_unmultiplied([w,h], self.rgba_frame.data_bytes().unwrap());
-            println!("img done {:?}", getNow() - start);
-
-            let texture = ctx.load_texture("Test image", img);
-            ui.image(texture.id(), texture.size_vec2());
-            println!("ui done {:?}", getNow() - start);
-
-        });
-
-
-
-    }
-
-    fn name(&self) -> &str {
-        return self.name.borrow();
-    }
-
-
-
 }
 
